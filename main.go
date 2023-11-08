f03114864913718ed363bf11c3dfb9af690a9d12
Lucas Popov <lucaspopov@protonmail.com>
2023-11-08 15:59:13 -0300

    feat: create login screen

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"log"
 	"net/http"
 	"webapp/src/router"
+	"webapp/src/utils"
 )
 
 func main() {
-	fmt.Println("rodando WebApp")
+	utils.CarregarTemplates()
 	r := router.Gerar()
+
+	fmt.Println("Escutando na porta 3000!")
 	log.Fatal(http.ListenAndServe("localhost:3000", r))
 }
--- src/controllers/login.go
+++ src/controllers/login.go
@@ -1,7 +1,10 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"webapp/src/utils"
+)
 
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Tela de login"))
+	utils.ExecutarTemplate(w, "login.html", nil)
 }
